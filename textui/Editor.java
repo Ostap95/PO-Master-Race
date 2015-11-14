ce52142ba7c5b81216374a21506bfd79cba414ec
Ostap <ostap_95@hotmail.com>
2015-11-14 00:03:41 +0000

    Menu added

--- textui/Editor.java
+++ textui/Editor.java
@@ -1,5 +1,10 @@
 package edt.textui;
 
+
+import edt.textui.main.MainMenu;
+import edt.core.Document;
+import pt.utl.ist.po.ui.Menu;
+import static pt.utl.ist.po.ui.UserInteraction.IO;
 /**
  * Themain class of the edt application.
  **/
@@ -7,5 +12,9 @@ public class Editor {
 
     public static void main(String[] args) {
         /* FIXME: Add code */
+		Document doc = new Document();
+		MainMenu menu = new MainMenu(doc);
+		menu.open();
+		IO.close();
     }
 }
