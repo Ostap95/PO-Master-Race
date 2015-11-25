e47e0fc1ab30af17ed9dced9631eaf33baa56dc8
Ostap <ostap_95@hotmail.com>
2015-11-25 18:09:10 +0000

    v 4.1 added IndexParagraph.java

--- textui/section/IndexParagraph.java
+++ textui/section/IndexParagraph.java
@@ -7,7 +7,8 @@ import pt.utl.ist.po.ui.InputInteger;
 import pt.utl.ist.po.ui.InputString;
 
 import edt.core.*;
-/* FIXME: import core classes here */
+import pt.utl.ist.po.ui.InvalidOperation;
+import java.util.HashMap;
 
 /**
  * Command for indexing a paragraph (nomear um parágrafo 2.2.9) of the current section.
@@ -33,6 +34,23 @@ public class IndexParagraph extends Command<Section> {
     @Override
     @SuppressWarnings("nls")
     public final void execute() {
-        /* FIXME: implement command */
+      Display display = new Display();
+      Form f = new Form();
+      InputInteger paragraphId = new InputInteger(f, Message.requestParagraphId());
+      InputString uniqueId = new InputString(f, Message.requestUniqueId());
+      f.parse();
+      try {
+          Paragraph desiredParagraph = entity().getParagraph(paragraphId.value());
+          HashMap<String, TextElement> elementMap = _doc.getElementMap();
+          if(elementMap.containsKey(uniqueId.toString())) {
+            desiredParagraph.setKey(uniqueId.toString());
+            display.add(Message.paragraphNameChanged());
+          } else {
+            _doc.indexElement(uniqueId.toString(), desiredParagraph);
+          }
+      } catch (InvalidOperation e) {
+          display.add(Message.noSuchParagraph(paragraphId.value()));
+          display.display();
+      }
     }
 }
